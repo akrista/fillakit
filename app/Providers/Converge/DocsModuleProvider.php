b1bf8220c3430941ab0430d1e6d8a58a81b75d12
Jorge Thomas (Akrista) <23145794+Akrista@users.noreply.github.com>
2025-09-26 20:21:31 -0400

    chore: converge updates

--- app/Providers/Converge/DocsModuleProvider.php
+++ app/Providers/Converge/DocsModuleProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers\Converge;
 
 use Converge\Enums\HighlighterName;
+use Converge\Enums\IconPosition;
 use Converge\Enums\Layout;
 use Converge\Enums\Spotlight;
 use Converge\MenuItems\MenuItem;
@@ -13,6 +14,7 @@ use Converge\Module;
 use Converge\Providers\ModuleProvider;
 use Converge\Support\Themes;
 use Converge\Theme\Theme;
+use Illuminate\Support\HtmlString;
 
 final class DocsModuleProvider extends ModuleProvider
 {
@@ -22,19 +24,31 @@ final class DocsModuleProvider extends ModuleProvider
     public function module(Module $module): Module
     {
         return $module
-            ->default()
             ->id('docs')
+            ->default()
+            ->latestVersionLabel('v1.0.0')
             ->routePath('docs')
             ->in(base_path('docs'))
-            ->brandLogo('Filamentry')
+            ->brandLogo('Fillakit')
             ->theme(fn(Theme $theme): Theme => $this->theme($theme))
-            ->defineMenuItems(function (MenuItems $menuItems): void {
-                $menuItems->add(
-                    fn (MenuItem $menuItem): MenuItem => $menuItem->url('https://github.com/akrista/filamentry')
-                        ->openUrlInNewTab()
-                        ->label('Github')
-                );
-            });
+            ->defineMenuItems(fn(MenuItems $menuItems) => $this->defineMenuItems($menuItems));
+    }
+
+    private function defineMenuItems(MenuItems $menuItems)
+    {
+        $menuItems->add(
+            fn (MenuItem $menuItem): MenuItem => $menuItem->url('https://github.com/akrista/fillakit')
+                ->openUrlInNewTab()
+                ->icon(fn() => new HtmlString('<svg class="w-5 h-5" viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg"><path clip-rule="evenodd" fill="currentColor" fill-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"/></svg>'))
+                ->iconPosition(IconPosition::After)
+                ->label('')
+        );
+        $menuItems->add(
+            fn(MenuItem $menuItem) => $menuItem->url('https://github.com/akrista/fillakit?sponsor=1')
+                ->openUrlInNewTab()
+                ->classes('btn btn-sm btn-outline btn-primary')
+                ->label('Sponsor')
+        );
     }
 
     private function theme(Theme $theme): Theme
