a10cb991922dbd5d83777a8b6b20c84db5004763
Jorge Thomas (Akrista) <23145794+Akrista@users.noreply.github.com>
2025-11-06 20:15:46 -0400

    chore: updating react starter kit

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 if (!config('fillakit.only_filament')) {
-    Route::get('/', fn(): Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View => view('welcome'));
+    Route::get('/', fn() => Inertia::render('welcome'))->name('home');
 }
