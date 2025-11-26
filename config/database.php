b806ec56042bb1c3657c0d89cd27c8ec3efb28cf
Jorge Thomas (Akrista) <23145794+Akrista@users.noreply.github.com>
2025-11-26 09:16:11 -0400

    fix: downgrade to php 8.4, working on docker support for wayfinder

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::ATTR_PERSISTENT => true,
             ]) : [],
         ],
@@ -82,7 +82,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::ATTR_PERSISTENT => true,
             ]) : [],
         ],
